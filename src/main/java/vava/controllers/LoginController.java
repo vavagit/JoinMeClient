20b9617a58a89921721d5c9c3604b997e2d1aa97
damianmajercak <damian.majercak@gmail.com>
2018-04-26 00:20:36 +0200

    login

---
+++ src/main/java/vava/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package vava.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
