2be35b653a4f70fa720c8846f6b266d4bd0fdd1c
damianmajercak <damian.majercak@gmail.com>
2018-05-08 18:48:33 +0200

    nejake zmeny

--- src/main/java/vava/app/controllers/EventDescriptionController.java
+++ src/main/java/vava/app/controllers/EventDescriptionController.java
@@ -1,13 +1,17 @@
 package vava.app.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,12 +19,13 @@ import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.Marker;
 import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import vava.app.Config;
 import vava.app.PropertyManager;
@@ -50,12 +55,14 @@ public class EventDescriptionController implements Initializable {
 	@FXML Button joinButton;
 	@FXML Pane gmaps;
 	Event event;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		GmComponent gm = GmComponent.getInstance();
 		gmaps.getChildren().add(gm.mapComponent);
 	}
+	
 	public void fillEventObject(Event event) {
 		this.event = event;
 		User user;
@@ -91,8 +98,34 @@ public class EventDescriptionController implements Initializable {
 		dataDescriptionLabel.setText(event.getDescription());
 		dataDateLabel.setText(event.getDate().toString());
 		dataCreatorLabel.setText(user.getUserName());
-		joinButton.setText(pm.getProperty("joinButton"));
 		
+		//joinButton.setText();
+		List<User> joinedUser = new ArrayList<>();
+		try {
+			ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+			RestTemplateFactory factory = context.getBean(RestTemplateFactory.class);
+			RestTemplate template = factory.getObject();
+			String ip = new PropertyManager("src/main/resources/connectionConfig").getProperty("host");
+			String url = "http://"+ip+":8009/events/" + event.getEventId() + "/users";
+			ResponseEntity<List<User>> returnedEntity = template.exchange(url, HttpMethod.GET, null,new ParameterizedTypeReference<List<User>>() {});
+			for(User u : returnedEntity.getBody()) {
+				System.out.println("nejde---------");
+				joinedUser.add(u);
+			}
+			((ConfigurableApplicationContext)context).close();
+		}
+		catch(RestClientException p){
+			p.printStackTrace();
+			return;
+		}
+		if(joinedUser.contains(Dataset.getInstance().getLoggedIn())) {
+			joinButton.setText(pm.getProperty("joinButtonJoined"));
+		}
+		else{
+			String buttontext = pm.getProperty("joinButton");
+			buttontext = buttontext + " ("+joinedUser.size()+"/"+event.getMaxUsersOnEvent()+")";
+			joinButton.setText(buttontext);
+		}
 		String language = "sk";
 		if("sk".equals(language)) {
 			dataSportCategoryLabel.setText(event.getSportCategory().getSport_sk());
@@ -105,6 +138,27 @@ public class EventDescriptionController implements Initializable {
 		GmComponent.getInstance().map.addMarker(new Marker(new MarkerOptions().position(l)));
 		GmComponent.getInstance().map.setCenter(l);
 		GmComponent.getInstance().map.setZoom(12);
+	}
+	
+	
+	@FXML
+	private void joinButtonHandle(ActionEvent event) {
+		//users/idUsera/event/ideventu
+		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		RestTemplate template = context.getBean(RestTemplate.class);
+		((ConfigurableApplicationContext) context).close();
+		
+		String ip = new PropertyManager(getClass().getResource("/connectionConfig").getFile()).getProperty("host");
+		final String url = "http://" + ip + ":8009/users/"+Dataset.getInstance().getLoggedIn().getId()+"/event/"+this.event.getEventId();
+		try {
+			template.postForEntity(url, null, Void.class);
+		} catch (HttpStatusCodeException e) {
+			new Alert(AlertType.ERROR, "Nepodarilo sa pripojic").showAndWait();
+			return;
+		} catch (RestClientException e) {
+			new Alert(AlertType.ERROR, "Chyba spojenia").showAndWait();
+			return;
+		}
 	}
 	
 
--- src/main/java/vava/app/model/User.java
+++ src/main/java/vava/app/model/User.java
@@ -121,4 +121,78 @@ public class User {
 				+ contact + ", address=" + address + ", addressLocation=" + addressLocation + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((addressLocation == null) ? 0 : addressLocation.hashCode());
+		result = prime * result + ((contact == null) ? 0 : contact.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + Float.floatToIntBits(rating);
+		result = prime * result + ((registeredAt == null) ? 0 : registeredAt.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (addressLocation == null) {
+			if (other.addressLocation != null)
+				return false;
+		} else if (!addressLocation.equals(other.addressLocation))
+			return false;
+		if (contact == null) {
+			if (other.contact != null)
+				return false;
+		} else if (!contact.equals(other.contact))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (Float.floatToIntBits(rating) != Float.floatToIntBits(other.rating))
+			return false;
+		if (registeredAt == null) {
+			if (other.registeredAt != null)
+				return false;
+		} else if (!registeredAt.equals(other.registeredAt))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+
 }
