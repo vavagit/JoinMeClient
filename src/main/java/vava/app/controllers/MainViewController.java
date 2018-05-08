4f5f69cf88b5935321cad6f1242ce19e8b5fbb59
erikmaruskin <erikmaruskin@gmail.com>
2018-05-08 17:34:53 +0200

    Multilanguage upadate

--- src/main/java/vava/app/controllers/MainViewController.java
+++ src/main/java/vava/app/controllers/MainViewController.java
@@ -30,6 +30,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -42,16 +43,12 @@ import vava.app.components.EventPaneComponent;
 import vava.app.components.GmComponent;
 import vava.app.model.Dataset;
 import vava.app.model.Event;
-import vava.app.model.User;
+import vava.app.model.Location;
 
 
 public class MainViewController implements Initializable {
 	private MainViewController mwc;
-	private LatLong l;
-	Stage s;
-	String leftTitle = "Hi!";
-	String nameUser = "Damian Majercak";
-	String locationLabelS = "Lokacia: ";
+	private LatLong location;
 	@FXML ListView<EventPaneComponent> eventListView;
 	@FXML Label titleLeftLabel;
 	@FXML Label nameOfUserLabel;
@@ -59,27 +56,38 @@ public class MainViewController implements Initializable {
 	@FXML TextField locationTextField;
 	@FXML Label rangeLabel;
 	@FXML TextField rangeTextField;
+	@FXML Hyperlink createEventLink;
+	@FXML Hyperlink myEventsLink;
+	@FXML Hyperlink joinedEventsLink;
 	@FXML Button filterButton;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		mwc = this;
-		titleLeftLabel.setText(leftTitle);
-		nameOfUserLabel.setText(nameUser);
-		locationLabel.setText(locationLabelS);
-		rangeLabel.setText("Range: ");
-       try {
+		//multilanguage
+		PropertyManager manager = new PropertyManager("");
+		manager.loadLanguageSet(getClass());
+		createEventLink.setText(manager.getProperty("createEventLabel"));
+		myEventsLink.setText(manager.getProperty("myEventsLabel"));
+		joinedEventsLink.setText(manager.getProperty("joinedEventsLabel"));
+		filterButton.setText(manager.getProperty("filterButton"));
+
+		//nastavenie nazvov podla jazyku
+		loadEvents(Dataset.getInstance().getLoggedIn().getAddressLocation(), 20);
+	}
+	
+	private void loadEvents(Location location, int radius) {
+		try {
      		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
      		RestTemplate template = context.getBean(RestTemplate.class);
      		((ConfigurableApplicationContext)context).close();     		
      		
      		final String ip = new PropertyManager("src/main/resources/connectionConfig").getProperty("host");
      		final String url = "http://" + ip + ":8009/events?lon={lon}&lat={lat}&radius={radius}";
-     		User loggedIn = Dataset.getInstance().getLoggedIn();
      		
      		Map<String, Object> map = new HashMap<>();
-     		map.put("lon", loggedIn.getAddressLocation().getLongitude());
-     		map.put("lat", loggedIn.getAddressLocation().getLatitude());
-     		map.put("radius", 10);
+     		map.put("lon", location.getLongitude());
+     		map.put("lat", location.getLatitude());
+     		map.put("radius", radius);
      		
      		ResponseEntity<List<Event>> returnedEntity = template.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Event>>() {}, map);
     		List<EventPaneComponent> list = new ArrayList<>();
@@ -92,8 +100,8 @@ public class MainViewController implements Initializable {
        catch(RestClientException e) {
      		e.printStackTrace();
      	}
-
 	}
+	
 	@FXML
 	private void mouseEnteredFilterBt(MouseEvent event) {
 		filterButton.setStyle("-fx-background-color: #6dcde9");
@@ -105,29 +113,30 @@ public class MainViewController implements Initializable {
 		filterButton.setStyle("-fx-background-color: #19b9e7");
 		//filterButton.getStyleClass().remove("buttonChange");
 	}
+	
 	@FXML 
 	private void inputHandle(KeyEvent event) {
 			String place = locationTextField.getText();
 			System.out.println("text-------- "+place);
 			GmComponent.getInstance().geocodingAddress(locationTextField.getText(), mwc);
 	}
+	
 	@FXML
 	private void createEventHandle(ActionEvent event) {
-		s = new Stage();
+		Stage newStage = new Stage();
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/vava/app/views/CreateEvents.fxml"));
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
-	        s.setScene(scene);
-	        s.show();
+			newStage.setScene(scene);
+			newStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
 		
 	}
-	
-	
+		
 	@FXML
 	private void filterHandle(ActionEvent event) {
 		String selectedArea = locationTextField.getText();
@@ -138,6 +147,7 @@ public class MainViewController implements Initializable {
 			new Alert(AlertType.ERROR, "Je nutne vyplnit rozsah vyhladavania filtra").showAndWait();
 			return;
 		}
+		
 		int range = 0;
 		try {
 			range = Integer.parseInt(rangeString);
@@ -146,44 +156,25 @@ public class MainViewController implements Initializable {
 			return;
 		}
 		
-		Double latitude = null ;
-		Double longitude =null;
+		double latitude = Dataset.getInstance().getLoggedIn().getAddressLocation().getLatitude();
+		double longitude =Dataset.getInstance().getLoggedIn().getAddressLocation().getLongitude();
 
 		if(!selectedArea.isEmpty()) {
-			if(l == null) {
+			if(location == null) {
 				new Alert(AlertType.ERROR, "Poloha nebola najdena").showAndWait();
 				return;
 			}
 			
-			latitude = l.getLatitude();
-			longitude = l.getLongitude();
-			System.out.println(l);
+			latitude = location.getLatitude();
+			longitude = location.getLongitude();
+			System.out.println(location);
 		}
 			
-		try {
-     		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-     		RestTemplate template = context.getBean(RestTemplate.class);
-     		((ConfigurableApplicationContext)context).close();     		
-     		String ip = new PropertyManager("src/main/resources/connectionConfig").getProperty("host");
-     		final String url = "http://"+ip+":8009/events?lon={lon}&lat={lat}&radius={radius}";
-     		Map<String, Object> map = new HashMap<>();
-     		map.put("lon", longitude);
-     		map.put("lat", latitude);
-     		map.put("radius", range);
-     		ResponseEntity<List<Event>> returnedEntity = template.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Event>>() {}, map);
-     		
-    		List<EventPaneComponent> list = new ArrayList<>();
-     		for(Event current : returnedEntity.getBody()) {
-     			list.add(new EventPaneComponent(current));
-     		}
-     		eventListView.setItems(FXCollections.observableList(list));
-        	
-     	}catch(RestClientException e) {
-     		e.printStackTrace();
-     	}
+		loadEvents(new Location(latitude,longitude), range);
 	}
+	
 	public void fillLongLitude(LatLong l) {
-		this.l = l;
+		this.location = l;
 		System.out.println("Naplnam polohu "+l);
 	}
 	
