f9aae9f5640f920113705a1f1bc789c69033e933
damianmajercak <damian.majercak@gmail.com>
2018-05-09 13:40:30 +0200

    uprava eventPaneComponent

--- src/main/java/vava/app/components/EventPaneComponent.java
+++ src/main/java/vava/app/components/EventPaneComponent.java
@@ -1,14 +1,28 @@
 package vava.app.components;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -16,11 +30,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import vava.app.Config;
 import vava.app.PropertyManager;
 import vava.app.controllers.EventDescriptionController;
+import vava.app.model.Dataset;
 import vava.app.model.Event;
+import vava.app.model.User;
+import vava.app.model.communication.RestTemplateFactory;
 
 public class EventPaneComponent extends HBox{
 	private ImageView img = new ImageView();
@@ -52,8 +71,29 @@ public class EventPaneComponent extends HBox{
     private Button buttonJoin = new Button();
     private Button detailBt = new Button();
     private VBox vboxBt = new VBox();
+    private List<User> joinedUser = new ArrayList<>();
     
      private void init() {
+    	 title.setWrapText(true);
+    	 buttonJoin.setMaxWidth(Double.MAX_VALUE);
+ 		try {
+ 			ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+ 			RestTemplateFactory factory = context.getBean(RestTemplateFactory.class);
+ 			RestTemplate template = factory.getObject();
+ 			String ip = new PropertyManager("src/main/resources/connectionConfig").getProperty("host");
+ 			String url = "http://"+ip+":8009/events/" + this.event.getEventId() + "/users";
+ 			ResponseEntity<List<User>> returnedEntity = template.exchange(url, HttpMethod.GET, null,new ParameterizedTypeReference<List<User>>() {});
+ 			for(User u : returnedEntity.getBody()) {
+ 				//System.out.println("nejde---------");
+ 				joinedUser.add(u);
+ 			}
+ 			((ConfigurableApplicationContext)context).close();
+ 		}
+ 		catch(RestClientException p){
+ 			p.printStackTrace();
+ 			return;
+ 		}
+ 		    	 
     	 detailBt.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent ev) {
@@ -82,13 +122,42 @@ public class EventPaneComponent extends HBox{
 				}
 			}
 		});
+    	 buttonJoin.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				//users/idUsera/event/ideventu
+				ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+				RestTemplate template = context.getBean(RestTemplate.class);
+				((ConfigurableApplicationContext) context).close();
+				
+				String ip = new PropertyManager(getClass().getResource("/connectionConfig").getFile()).getProperty("host");
+				final String url = "http://" + ip + ":8009/users/"+Dataset.getInstance().getLoggedIn().getId()+"/event/"+EventPaneComponent.this.event.getEventId();
+				try {
+					template.postForEntity(url, null, Void.class);
+					PropertyManager pm = new PropertyManager("");
+					pm.loadLanguageSet(getClass().getSimpleName());
+					buttonJoin.setText(pm.getProperty("buttonJoined"));
+					buttonJoin.setDisable(true);
+				} catch (HttpStatusCodeException e) {
+					new Alert(AlertType.ERROR, "Nepodarilo sa pripojic").showAndWait();
+					return;
+				} catch (RestClientException e) {
+					new Alert(AlertType.ERROR, "Chyba spojenia").showAndWait();
+					return;
+				}
+				
+			}
+    		 
+		});
+    	 
      }
      
     public EventPaneComponent(Event e) {
 		super();
+		event = e;
 		init();
 		//init
-		event = e;
+		
 		title.getStyleClass().add("vbLabel");
 		date.getStyleClass().add("vbLabel");
 		address.getStyleClass().add("vbLabel");
@@ -102,14 +171,19 @@ public class EventPaneComponent extends HBox{
 		img.setImage(im);
 		img.setFitWidth(50);
 	    img.setFitHeight(50);
+	    title.setTextAlignment(TextAlignment.LEFT);
+	    title.setAlignment(Pos.CENTER_LEFT);
 	    vboxPicWithTitle.getChildren().addAll(this.title,img);
+	    vboxPicWithTitle.setAlignment(Pos.CENTER_LEFT);
+	    vboxPicWithTitle.setMaxWidth(100);
+	    vboxPicWithTitle.setMinWidth(100);
 	    //------------------
 	    //mozno by sa hodil prazdny
 	    //-----druhy vbox---------
 	    PropertyManager manager = new PropertyManager("");
 	    manager.loadLanguageSet(getClass().getSimpleName());
 	    numberOfUsers.setText(manager.getProperty("numberOfUsers"));
-	    numberOfUsersValue.setText("0/"+e.getMaxUsersOnEvent()); // dorob pocet uzivatelov na dany event
+	    numberOfUsersValue.setText(joinedUser.size()+"/"+e.getMaxUsersOnEvent()); // dorob pocet uzivatelov na dany event
 	    infoR1.getChildren().addAll(numberOfUsers,numberOfUsersValue);
 	    date.setText(manager.getProperty("date"));
 	    dateValue.setText(e.getDate().toString());
@@ -118,6 +192,19 @@ public class EventPaneComponent extends HBox{
 	    valueOfAddress.setText(e.getAddress());
 	    infoR3.getChildren().addAll(address,valueOfAddress);
 	    
+		if(joinedUser.contains(Dataset.getInstance().getLoggedIn())) {
+ 			buttonJoin.setText(manager.getProperty("buttonJoined"));
+ 			buttonJoin.setDisable(true);
+ 		}
+ 		else{
+ 			String buttontext = manager.getProperty("buttonJoin");
+ 			System.out.println(buttontext+"---");
+ 			
+ 			buttonJoin.setText(buttontext);
+ 		}
+	    
+	    
+	    
 	    info.getChildren().addAll(infoR1,infoR2,infoR3);
 	    
 	    // treti vbox
@@ -125,7 +212,6 @@ public class EventPaneComponent extends HBox{
 	    buttonJoin.getStyleClass().add("buttonJoin");
 	    Pane p = new Pane();
 	    p.setPrefHeight(5);
-	    buttonJoin.setText("Join");
 	    vboxBt.getChildren().addAll(buttonJoin,p,detailBt);
 	    Pane space = new Pane();
 	    space.setPrefWidth(30);
@@ -133,7 +219,9 @@ public class EventPaneComponent extends HBox{
 	    space.setMaxWidth(Double.MAX_VALUE);
 	    EventPaneComponent.setHgrow(space2, Priority.ALWAYS);
 	    vboxBt.setAlignment(Pos.CENTER_RIGHT);
-	    this.setAlignment(Pos.CENTER_RIGHT);
+	    vboxBt.setMaxWidth(95);
+	    vboxBt.setMinWidth(95);
+	    //this.setAlignment(Pos.CENTER_RIGHT);
 	    this.getChildren().addAll(vboxPicWithTitle,space,info,space2 ,vboxBt);
      }
 }
